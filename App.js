0eeee368dc1880a4e94e5c2d760def970c0b21de
Covee <uniqtop91@gmail.com>
2018-09-08 22:19:51 +0900

    complete, uncomplete functions

--- App.js
+++ App.js
@@ -46,7 +46,13 @@ export default class App extends React.Component {
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             {Object.values(toDos).map(toDo => (
-              <ToDo key={toDo.id} deleteToDo={this._deleteToDo} {...toDo} />
+              <ToDo 
+                key={toDo.id} 
+                deleteToDo={this._deleteToDo}
+                uncompleteToDo={this._uncompleteToDo}
+                completeToDo={this._completeToDo}
+                {...toDo}
+              />
             ))}
           </ScrollView>
         </View>
@@ -99,6 +105,36 @@ export default class App extends React.Component {
       return { ...newState };
     });
   };
+  _uncompleteToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: false,
+          }
+        }
+      }
+      return { ...newState };
+    })
+  }
+  _completeToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: true,
+          }
+        }
+      }
+      return { ...newState };
+    })
+  }
 }
 
 const styles = StyleSheet.create({
